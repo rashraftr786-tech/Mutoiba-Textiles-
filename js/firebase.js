9871109f3b92e941a313756e826354d5af88b0fe
rashraftr786-tech <rashraftr786@gmail.com>
2025-12-25 23:45:25 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,17 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-  const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "XXXXXX",
-    appId: "XXXXXX"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDFcVgtofCuu3uQpMiofAPm3JQ0xiaeJVQ",
+  authDomain: "mutoiba-textiles.firebaseapp.com",
+  projectId: "mutoiba-textiles",
+  storageBucket: "mutoiba-textiles.appspot.com",
+  messagingSenderId: "544691020034",
+  appId: "1:544691020034:web:82ee6c0f8e94256fbce21b"
+};
 
-  const app = initializeApp(firebaseConfig);
-  window.auth = getAuth(app);
-  window.db = getFirestore(app);
-</script>
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
