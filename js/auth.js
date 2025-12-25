fd3083f6a56cb5fa23badedff37b9428bad2345e
rashraftr786-tech <rashraftr786@gmail.com>
2025-12-25 20:39:21 +0530

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,13 @@
+import { signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => alert(err.message));
+};
